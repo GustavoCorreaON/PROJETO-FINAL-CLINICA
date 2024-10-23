6422ce9c42ae6975a37b29b07315fb9d6725b3e2
anasola-06 <anaclara.sola@aluno.senai.br>
2024-10-23 15:33:09 -0300

    Update controllerCadasMed.php

--- ProjetoClinica/siteOf-main/controllers/controllerCadasMed.php
+++ ProjetoClinica/siteOf-main/controllers/controllerCadasMed.php
@@ -14,8 +14,11 @@ class BookController {
     public function dados() {
         // Recebe dados do formulário
         $nome = $_POST['nomecompleto'];
+        $numcrm = $_POST['numerocrm'];
+        $espec = $_POST['especializacao'];
         $cpf = $_POST['cpf'];
         $email = $_POST['email'];
+        $senha = $_POST['senha'];
         $datanascimento = $_POST['datanascimento'];
         $emailclinica = $_POST['emailclinica'];
         $confirmeemailclinica = $_POST['confirmeemailclinica'];
