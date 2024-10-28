25d236403e0061e9d9160de85176074bc047c335
anasola-06 <anaclara.sola@aluno.senai.br>
2024-10-28 08:13:52 -0300

    Update controllerCadasPac.php

--- ProjetoClinica/siteOf-main/controllers/controllerCadasPac.php
+++ ProjetoClinica/siteOf-main/controllers/controllerCadasPac.php
@@ -2,7 +2,7 @@
 
 require_once '../models/Book.php';
 
-class CadastroAdm {
+class CadastroPac {
 
     public function showForm() {
         // Exibe o formulário de cadastro de livros
@@ -16,10 +16,14 @@ class CadastroAdm {
         $plano = $_POST['planosimounao'];
         $qual = $_POST['qualplano'];
         $sexo = $_POST['femoumasc'];
+        $numerocart = $_POST['numerocarteirinha'];
+        $datanascimento = $_POST['datanascimento'];
         $nome = $_POST['nomecompleto'];
         $cpf = $_POST['cpf'];
+        $grauparentes = $_POST['graudeparentesco'];
+        $nomepaciente = $_POST['nomedopaciente'];
+        $cpfpaciente = $_POST['cpfdopaciente'];
         $email = $_POST['email'];
-        $datanascimento = $_POST['datanascimento'];
         $emailclinica = $_POST['emailclinica'];
         $confirmeemailclinica = $_POST['confirmeemailclinica'];
         $senhaclinica = $_POST['senhaclinica'];
