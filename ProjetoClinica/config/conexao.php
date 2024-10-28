b520abb50c93e523a2863c616c00a2b529fd0972
ju-senai <juliarocha@aluno.senai.br>
2024-10-28 14:37:42 -0300

    Update conexao.php

--- ProjetoClinica/config/conexao.php
+++ ProjetoClinica/config/conexao.php
@@ -12,10 +12,12 @@ class Database{
     
 
     try {
-        $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+        $this->conn = new PDO("mysql:host=" . $this->host . ";db_name=" . $this->db_name, $this->username, $this->password);
         $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch (PDOException $exception){
         echo "Connection error: " . $exception->getMessage();
     }
+
+    return $this->conn;
 }
 }
