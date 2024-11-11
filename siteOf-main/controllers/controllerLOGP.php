0b3675e4e3a90d3b3a59d09eb32fb2fa21bcff25
ju-senai <juliarocha@aluno.senai.br>
2024-11-11 08:52:04 -0300

    login paciente

--- siteOf-main/controllers/controllerLOGP.php
+++ siteOf-main/controllers/controllerLOGP.php
@@ -1,3 +1,16 @@
 <?php
 
 require_once '../model/modelLOGP.php';
+
+class LpacienteController {
+    public function showCP() {
+        require_once '../views/html/Lpaciente.php';
+    }
+    
+
+    public function logarPaciente() {
+        // Recebe dados do formulário
+        $nomepaciente = $_POST ['nomepaciente'];
+        $cpf = $_POST['cpf'];
+        $senha = $_POST ['senha'];
+}}
