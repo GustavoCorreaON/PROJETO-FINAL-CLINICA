9051b640ce2e8d2a07554af8a2155f34d7f60fa9
GustavoCorreaON <gustavocorrea@aluno.senai.br>
2024-12-02 09:02:14 -0300

    a

--- siteOf-main/config/conexao.php
+++ siteOf-main/config/conexao.php
@@ -1,22 +1,23 @@
 <?php
 
-class Database {
+class Database{
     private $host = 'localhost';
     private $db_name = 'sp_medical_group';
     private $username = 'root';
     private $password = 'root';
     public $conn;
 
-    public function getConnection() {
+    public function getConnection(){
         $this->conn = null;
+    
 
-        try {
-            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
-            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        } catch (PDOException $exception) {
-            echo "Connection error: " . $exception->getMessage();
-        }
-
-        return $this->conn;
+    try {
+        $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+        $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $exception){
+        echo "Connection error: " . $exception->getMessage();
     }
+
+    return $this->conn;
+}
 }
